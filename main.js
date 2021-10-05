b4d87d38a77e2289f27b4cd4efd38adf4be01779
domokosdavid <77434649+domokosdavid@users.noreply.github.com>
2021-10-05 17:35:09 +0200

    tablazat stilus

--- main.js
+++ main.js
@@ -5,31 +5,30 @@ class Film {
       this.kiadasiEv = kiadasiEv;
     }
   }
-
 var filmAdatai = [];
+
 
 function hozzaadFunction(){
     let nev = document.getElementById("name").value;
     let rendezo = document.getElementById("rendezo").value;
-    let kiadas = document.getElementById("kiadas").value;
-    filmAdatai.push(nev,rendezo,kiadas);
+    let kiadasiEv = document.getElementById("kiadas").value;
+    let f1 = new Film(nev,rendezo,kiadasiEv);
+    filmAdatai.push(f1);
     console.log(filmAdatai);
-    
 }
+
 function listazFunction(){
-    let check = document.getElementById("tablazat").innerHTML;
+    let check = document.getElementById("tablazatom").innerHTML;
     let tablazat = "";
     if(check == ""){
         let tablazat = "<table><tr><th>Név</th><th>Rendező</th><th>Kiadási év</th></tr>";
-        for(let i = 0; i<filmAdatai.length; i++){
-            tablazat += "<tr><td>"+filmek[i].nev+"</td>";
-            tablazat += "<tr><td>"+filmek[i].rendezo+"</td>";
-            tablazat += "<tr><td>"+filmek[i].kiadas+"</td></tr>";
-        }
-        tablazat += "</table>";
-        document.getElementById("tablazat").innerHTML = tablazat;
+            for (let j = 0; j < filmAdatai.length; j++){
+                tablazat += "<tr><td>"+filmAdatai[j].nev+"</td>"+"<td>"+filmAdatai[j].rendezo+"</td>"+"<td>"+filmAdatai[j].kiadasiEv+"</td></tr>";
+            }
+        tablazat +="</table>";
+        document.getElementById("tablazatom").innerHTML = tablazat;
     }
     else {
-        document.getElementById("tablazat").innerHTML = "";
+        document.getElementById("tablazatom").innerHTML = "";
     }
 }
